1fcfef725ba3bcfca6e353b7b5963e894933436a
Sebastien Aubin <spicheaubin@devolutions.net>
2020-02-06 08:39:52 -0500

    Test remove nil check

--- packages/firebase_messaging/ios/Classes/FLTFirebaseMessagingPlugin.m
+++ packages/firebase_messaging/ios/Classes/FLTFirebaseMessagingPlugin.m
@@ -139,9 +139,9 @@ static NSObject<FlutterPluginRegistrar> *_registrar;
   } else if ([@"configure" isEqualToString:method]) {
     [FIRMessaging messaging].shouldEstablishDirectChannel = true;
     [[UIApplication sharedApplication] registerForRemoteNotifications];
-    if (_launchNotification != nil) {
+    //if (_launchNotification != nil) {
       [_channel invokeMethod:@"onLaunch" arguments:_launchNotification];
-    }
+    //}
     result(nil);
   } else if ([@"subscribeToTopic" isEqualToString:method]) {
     NSString *topic = call.arguments;
